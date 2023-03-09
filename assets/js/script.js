2468bb617b8f22f78f4bcdd79e00b58018a5e420
am.azad66@gmail.com <am.azad66@gmail.com>
2023-03-09 17:06:48 +0330

    fix bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,42 @@
 const showLangtMenu = document.querySelector('#show-lang-menu');
 const LangIcon = document.querySelector('.lang-icon');
 const langDropDown = document.querySelector('.lang-drop-down');
+const SearchIcon = document.querySelector('.search-icon');
+const autocompleteInput = document.querySelector('.autocomplete');
+const autocompleteItems = document.querySelector('.autocomplete-items');
+LangIcon.addEventListener('click', function () {
+    if (langDropDown.style.opacity) {
+        langDropDown.style.animation = 'showUp 0.35s 1';
+        langDropDown.style.opacity = null;
+    } else {
+        langDropDown.style.animation = 'showUp 0.35s 1';
+        langDropDown.style.opacity = '1';
+        autocompleteInput.classList.remove('autocompleteHover')
+    }
+});
 
-LangIcon.addEventListener('mouseenter', function () {
-    
-  langDropDown.style.animation = 'showUp 0.35s 1';
-  langDropDown.style.opacity = '1';
-  div.style.display = 'block';
-  });
+window.addEventListener('click', function (e) {
+    if (!langDropDown.contains(e.target)) {
+        if (langDropDown.style.opacity) {
+            // langDropDown.style.animation = 'showUp 0.35s 1';
+            langDropDown.style.opacity = null;
+        }
+    }
+} ,true);
 
-  LangIcon.addEventListener('mouseleave', function () {
-    langDropDown.style.opacity = '0';
-    div.style.display = 'none';
-  });
-  
+SearchIcon.addEventListener('click', function (e) {
+    if(autocompleteInput.classList.contains('autocompleteHover')){
+        if (!autocompleteInput.contains(e.target) || !autocompleteItems.contains(e.target) ) {
+            autocompleteInput.classList.remove('autocompleteHover')
+        }
+    }else{
+        autocompleteInput.classList.add('autocompleteHover')
+        // langDropDown.style.animation = 'showUp 0.35s 1';
+        langDropDown.style.opacity = null;
+    }
+});
+
+
+let countries = ['ترانسفر فرودگاهی'];
+autocomplete(document.getElementById("myInput"), countries);
+
