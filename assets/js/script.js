64628e6a028b8d6bb008191f525bf2d09b0545d7
abdolmajid azad <abdolmajidazad@users.noreply.github.com>
2023-03-13 15:39:59 +0330

    step2

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const SearchIcon = document.querySelector('.search-icon');
 const autocompleteInput = document.querySelector('.autocomplete');
 const autocompleteItems = document.querySelector('.autocomplete-items');
 window.addEventListener('click', function (e) {
-    if(LangIcon.contains(e.target)){
+    if (LangIcon.contains(e.target)) {
         if (langDropDown.style.opacity) {
             langDropDown.style.animation = 'showUp 0.35s 1';
             langDropDown.style.opacity = null;
@@ -14,20 +14,20 @@ window.addEventListener('click', function (e) {
             langDropDown.style.opacity = '1';
             autocompleteInput.classList.remove('autocompleteHover')
         }
-    }else{
-        if(!langDropDown.contains(e.target)){
+    } else {
+        if (!langDropDown.contains(e.target)) {
             langDropDown.style.opacity = null;
         }
 
     }
-} );
+});
 
 SearchIcon.addEventListener('click', function (e) {
-    if(autocompleteInput.classList.contains('autocompleteHover')){
-        if (!autocompleteInput.contains(e.target) || !autocompleteItems.contains(e.target) ) {
+    if (autocompleteInput.classList.contains('autocompleteHover')) {
+        if (!autocompleteInput.contains(e.target) || !autocompleteItems.contains(e.target)) {
             autocompleteInput.classList.remove('autocompleteHover')
         }
-    }else{
+    } else {
         autocompleteInput.classList.add('autocompleteHover')
         // langDropDown.style.animation = 'showUp 0.35s 1';
         langDropDown.style.opacity = null;
@@ -38,3 +38,43 @@ SearchIcon.addEventListener('click', function (e) {
 let countries = ['ترانسفر فرودگاهی'];
 autocomplete(document.getElementById("myInput"), countries);
 
+
+function GetReserve(e, type) {
+    console.log("e, type::GetReserve:", e, type)
+}
+
+let formData = {};
+let requireField = ['date1', 'fligtNumber1', 'passenger1', 'cip1']
+function GetFlight(e, type) {
+    if (type === 'flight2') {
+        document.getElementById('flight2Section').style.display = 'flex'
+    } else {
+        document.getElementById('flight2Section').style.display = 'none'
+    }
+    document.getElementById('form-submit-btn').disabled = true;
+    requireField.forEach(item=>{
+        console.log("document.getElementsByName(item)", document.getElementsByName(item))
+        document.getElementsByName(item)[0].value = null
+    });
+    formData={}
+
+}
+
+;
+function GetFormInfo(e) {
+    formData[e.target.name] = e.target.value;
+
+    if (document.getElementById('flight2Section').style.display === 'flex') {
+        requireField = [...requireField, 'date2', 'fligtNumber2', 'passenger2', 'cip2'];
+    }
+
+    let disabled = false;
+    requireField.forEach(item => {
+        if (!formData[item]) {
+             disabled = true;
+        }
+    });
+    document.getElementById('form-submit-btn').disabled = disabled;
+
+    console.log("e, type::GetFormInfo:", e.target.name, e.target.value, document.getElementById('flight2Section').style.display)
+}
