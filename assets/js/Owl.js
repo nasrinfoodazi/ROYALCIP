aad5637b3e63af51397452dfcbfc16823cee6728
am.azad66@gmail.com <am.azad66@gmail.com>
2023-04-07 19:36:59 +0330

    update carucel

--- assets/js/Owl.js
+++ assets/js/Owl.js
@@ -5,11 +5,14 @@ $(document).ready(function() {
     var syncedSecondary = true;
     sync1.owlCarousel({
         items: 1,
-        slideSpeed: 2000,
-        nav: true,
+        slideSpeed: 0,
+        nav: false,
         autoplay: false,
-        dots: true,
-        loop: true,
+        animateOut: 'slideDown',
+        animateIn: 'flipInX',
+        rtl:true,
+        dots: false,
+        loop: false,
         responsiveRefreshRate: 200,
         navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     }).on('changed.owl.carousel', syncPosition);
@@ -19,10 +22,13 @@ $(document).ready(function() {
             sync2.find(".owl-item").eq(0).addClass("current");
         })
         .owlCarousel({
-            items: slidesPerPage,
-            dots: true,
-            nav: true,
-            smartSpeed: 200,
+            items: 6,
+            dots: false,
+            nav: false,
+            smartSpeed: 0,
+            margin: 10,
+            rtl:true,
+            loop: false,
             slideSpeed: 500,
             slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
             responsiveRefreshRate: 100
@@ -72,6 +78,13 @@ $(document).ready(function() {
     sync2.on("click", ".owl-item", function(e) {
         e.preventDefault();
         var number = $(this).index();
-        sync1.data('owl.carousel').to(number, 300, true);
+        sync1.data('owl.carousel').to(number, 0, true);
+        var elems = document.querySelectorAll(".activeThumbnail");
+
+        [].forEach.call(elems, function(el) {
+            el.classList.remove("activeThumbnail");
+        });
+        let add =  document.getElementById('thumbnail-'+(number+1));
+        add.classList.add('activeThumbnail')
     });
 });
